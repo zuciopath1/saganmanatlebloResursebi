f16c2ef23e1be00af869e2d071f1cfa0f867fed0
zuciopath <73157609+zuciopath@users.noreply.github.com>
2023-02-19 13:13:36 +0400

    some changes
    There is now an hteccess file. New meta tags have been added to index.htm, including apple-touch-icon, which at the moment lacks content.meta description, and meta-theme-color. I've switched the nav and ul tags' directions in the header. serviceWorker.js and the manifest.json file were added.

---
+++ sw.js
@@ -0,0 +1,44 @@
+// Register the service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+          console.log('Service worker registered successfully:', registration);
+        })
+        .catch(error => {
+          console.log('Service worker registration failed:', error);
+        });
+    });
+  }
+  
+  // Install and activate the service worker
+  self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('static')
+        .then(cache => {
+          cache.addAll([
+            '/',
+            '/index.html',
+            '/styles.css/style.min.css',
+            '/js/app.js',
+            '/favicon.png'
+          ]);
+        })
+    );
+  });
+  
+  // Fetch resources from the cache if available, or the network if not
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          if (response) {
+            console.log('Serving from cache:', event.request.url);
+            return response;
+          }
+          console.log('Fetching from network:', event.request.url);
+          return fetch(event.request);
+        })
+    );
+  });
+  
