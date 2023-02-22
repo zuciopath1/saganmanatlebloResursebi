96f718218ae7f328ae9dd0b57538694316af626c
zuciopath <73157609+zuciopath@users.noreply.github.com>
2023-02-22 19:16:15 +0400

    small bug fix. serviceworker temporarily commented

--- sw-register.js
+++ sw-register.js
@@ -1,11 +1,12 @@
 // if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/sw.js')
-//       .then(registration => {
-//         console.log('Service worker registered:', registration);
-//       })
-//       .catch(error => {
-//         console.log('Service worker registration failed:', error);
-//       });
-//   });
-// }
+//     window.addEventListener('load', () => {
+//       navigator.serviceWorker.register('/sw.js')
+//         .then((registration) => {
+//           console.log('Service worker registered successfully:', registration);
+//         })
+//         .catch((error) => {
+//           console.log('Service worker registration failed:', error);
+//         });
+//     });
+//   }
+  
--- sw.js
+++ sw.js
@@ -1,47 +1,47 @@
-// Register the service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    // Register the service worker and specify its scope
-    navigator.serviceWorker.register('https://zuciopath1.github.io/saganmanatlebloResursebi/sw.js', { scope: 'https://zuciopath1.github.io/saganmanatlebloResursebi/' })
-      .then(registration => {
-        console.log('Service worker registered successfully:', registration);
-      })
-      .catch(error => {
-        console.log('Service worker registration failed:', error);
-      });
-  });
-}
+// // Register the service worker
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     // Register the service worker and specify its scope
+//     navigator.serviceWorker.register('https://zuciopath1.github.io/saganmanatlebloResursebi/sw.js', { scope: 'https://zuciopath1.github.io/saganmanatlebloResursebi/' })
+//       .then(registration => {
+//         console.log('Service worker registered successfully:', registration);
+//       })
+//       .catch(error => {
+//         console.log('Service worker registration failed:', error);
+//       });
+//   });
+// }
 
-// Install and activate the service worker
-self.addEventListener('install', event => {
-  event.waitUntil(        
-    caches.open('static')
-      .then(cache => {
-        // Add files to the cache
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/static/styles/css/style.css',
-          '/static/js/app.js',
-          // '/favicon.png'
-        ]);
-      })
-  );
-});
+// // Install and activate the service worker
+// self.addEventListener('install', event => {
+//   event.waitUntil(        
+//     caches.open('static')
+//       .then(cache => {
+//         // Add files to the cache
+//         cache.addAll([
+//           '/',
+//           '/index.html',
+//           '/static/styles/css/style.css',
+//           '/static/js/app.js',
+//           // '/favicon.png'
+//         ]);
+//       })
+//   );
+// });
 
-// Fetch resources from the cache if available, or the network if not
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          // Serve the response from the cache
-          console.log('Serving from cache:', event.request.url);
-          return response;
-        }
-        // Fetch the resource from the network
-        console.log('Fetching from network:', event.request.url);
-        return fetch(event.request);
-      })
-  );
-});
+// // Fetch resources from the cache if available, or the network if not
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(response => {
+//         if (response) {
+//           // Serve the response from the cache
+//           console.log('Serving from cache:', event.request.url);
+//           return response;
+//         }
+//         // Fetch the resource from the network
+//         console.log('Fetching from network:', event.request.url);
+//         return fetch(event.request);
+//       })
+//   );
+// });
