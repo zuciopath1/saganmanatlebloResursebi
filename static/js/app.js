93ba16ca994acc03ddb01f0e69ca9ea36565daba
gio <giorgimakasarashvili@gmail.com>
2023-03-04 00:32:26 +0400

    added sheavse-chasvi

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,9 @@
 import booksData from './Books.json' assert {type: 'json'};
-import { ShowBooks, showLesson, showLessonSection, toggleParags } from "./function.js";
+import { ShowBooks, showLesson, showLessonSection, toggleParags, didiMogzauriSheavse } from "./function.js";
 
 ShowBooks(books, booksData);
 
 window.showLesson = showLesson;
 window.showLessonSection = showLessonSection;
 window.toggleParags = toggleParags;
+window.didiMogzauriSheavse = didiMogzauriSheavse;
